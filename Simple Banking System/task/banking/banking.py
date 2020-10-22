adef8e9c9b69bb3ec33d8b5ed94c48f1078ca4d1
IgnatIvanov <ignativanov1996@mail.ru>
2020-10-22 13:25:05 +0300

    2020.10.22.1324 Stage 2 complete.

--- Simple Banking System/task/banking/banking.py
+++ Simple Banking System/task/banking/banking.py
@@ -21,8 +21,25 @@ while option != 0:
     if option == 1:  # Create new account option
         card = "400000"
         while True:
-            for i in range(10):
+            for i in range(9):
                 card += str(random.randrange(10))
+            check_list = []
+            check_sum = 0
+            sum = 0
+            for x in range(15):
+                check_list.append(int(card[x]))
+            for x in range(15):
+                if (x + 1) % 2 == 1:
+                    check_list[x] *= 2
+            for x in range(15):
+                if check_list[x] > 9:
+                    check_list[x] -= 9
+            for x in range(15):
+                sum += check_list[x]
+            check_sum = 10 - (sum % 10)
+            if check_sum == 10:
+                check_sum = 0
+            card += str(check_sum)
             if card not in card_list:
                 break
         PIN = ""
